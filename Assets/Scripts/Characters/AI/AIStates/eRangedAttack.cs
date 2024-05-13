6128dd0cf2e02c3eaf6eeee929b9e1e80064e2c0
Renee Camillo Pollet <r.camillopollet798@mybvc.ca>
2024-05-13 16:37:46 -0600

    CineMachine and initial AI Scripts

---
+++ Assets/Scripts/Characters/AI/AIStates/eRangedAttack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eRangedAttack : BaseState
+{
+    // Start is called before the first frame update
+    public override void EnterState()
+    {
+
+    }
+    public override void ExitState()
+    {
+
+    }
+
+    public override void StateFixedUpdate()
+    {
+
+    }
+
+    public override void StateUpdate()
+    {
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Characters/BaseState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState
+{
+
+    protected float nextDirection;
+
+    public CharacterClass character { get; set; }
+    //public InputManager inputManager { get; set; }
+
+    protected BaseState currentState;
+
+    public virtual void EnterState() { }
+    public virtual void ExitState() { }
+    public virtual void StateFixedUpdate() { }
+    public virtual void StateUpdate() { }
+    public virtual void HandleMovement() { }
+    public virtual void HandleRotation(float rotateAngle) { }
+    public virtual void HandleInteract() { }
+    public virtual void HandleDeath() { }
+    public virtual void StopInteract() { }
+
+}
