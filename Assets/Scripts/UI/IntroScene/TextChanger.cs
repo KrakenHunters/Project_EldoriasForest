dfafe712888650fa1e471bda82981c12a165b88e
Renee Camillo Pollet <renee_p_35@hotmail.com>
2024-06-30 16:53:50 -0600

    AI Sound for Intro Scene

--- Assets/Scripts/UI/IntroScene/TextChanger.cs
+++ Assets/Scripts/UI/IntroScene/TextChanger.cs
@@ -4,9 +4,20 @@ using TMPro;
 public class TextChanger : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
+    private AudioSource audioSource;
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void ChangeText(string newText)
     {
         textComponent.text = newText;
+        
+    }
+
+    public void ChangeAudio(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
     }
 }
