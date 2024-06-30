bb06a05e66990630a3241b1570f6c24f6a2e3674
Renee Camillo Pollet <renee_p_35@hotmail.com>
2024-06-30 17:48:53 -0600

    IntroScene

--- Assets/Scripts/UI/IntroScene/TextChanger.cs
+++ Assets/Scripts/UI/IntroScene/TextChanger.cs
@@ -1,23 +1,48 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TextChanger : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    private AudioSource audioSource;
+
+    private int index;
+
+    private string[] IntroTexts = new string[]
+   {
+        "In the mystical village of Eldoria, the enchanting souls of forest creatures breathe life into the land.",
+        "But on one fateful day, a witch appeared, severing the connection between the animals and Eldoria.",
+        "Driven by a dark hunger, the witch corrupted the souls of the forest creatures, gathering their essence and casting Eldoria into despair!",
+        "Now the witch feeds on the forest, shaping it as she wishes and making life miserable for the people of Eldoria.",
+        "Legend speaks of a magician capable of defeating the witch and bringing peace back to Eldoria... Maybe you?",
+        "If not... we will just hire a knight or something."
+   };
+
+    [SerializeField]
+    private Sprite[] images;
+
+    [SerializeField]
+    private Image sceneImage;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        index = 0;
+        textComponent.text = IntroTexts[index];
+        sceneImage.sprite = images[index];
     }
-    public void ChangeText(string newText)
+    public void Change()
     {
-        textComponent.text = newText;
-        
+        index++;
+        textComponent.text = IntroTexts[index];
+        if (index <= IntroTexts.Length)
+            sceneImage.sprite = images[index];
+
     }
 
-    public void ChangeAudio(AudioClip audioClip)
+    public void StartGame()
     {
-        audioSource.clip = audioClip;
+        SceneManager.LoadScene("02_ForestScene");
     }
+
 }
