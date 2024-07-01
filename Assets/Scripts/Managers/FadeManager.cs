28f4a2389644aa7cb770e432c8eb12d3ea0c8964
Sean Lynch <seanzolynch@gmail.com>
2024-07-01 00:47:55 -0600

    Fading

--- Assets/Scripts/Managers/FadeManager.cs
+++ Assets/Scripts/Managers/FadeManager.cs
@@ -6,15 +6,17 @@ public class FadeManager : Singleton<FadeManager>
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform cam; 
-    [SerializeField] private LayerMask Layer; 
-
+    [SerializeField] private LayerMask Layer;
+    private ObjectFader[] fadeableObjects;
     private ObjectFader currentObjectToFade;
+
     private void Awake()
     {
         cam = Camera.main.transform;
         Layer = LayerMask.GetMask("Object");
+        fadeableObjects = FindObjectsOfType<ObjectFader>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         FadeObject();
     }
@@ -23,22 +25,31 @@ public class FadeManager : Singleton<FadeManager>
     {
         Vector3 playerPos = player.position;
 
-        Ray ray = Camera.main.ScreenPointToRay(playerPos);
+        Ray ray = new Ray(Camera.main.transform.position, (player.position - Camera.main.transform.position).normalized);
         RaycastHit hit;
-         
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, Layer))
+        RaycastHit[] hits = Physics.SphereCastAll(ray,2.0f, Mathf.Infinity, Layer);
+        foreach (ObjectFader fader in fadeableObjects)
+            fader.ShouldFade = false;
+        foreach (RaycastHit aHit in hits)
         {
-         
-                Debug.Log("Fading object start stufffffffr");
-
-                if (currentObjectToFade != null)
-                {
-                    currentObjectToFade.ResetFade();
-                }
-
-                currentObjectToFade = hit.collider.GetComponent<ObjectFader>();
-                currentObjectToFade.Fade();
+            Debug.Log("Hit me");
+            var fader = aHit.collider.GetComponent<ObjectFader>();
+            fader.ShouldFade = true;
         }
+
+        foreach (ObjectFader fader in fadeableObjects)
+        {
+            if(fader.ShouldFade)
+                fader.Fade();
+            else 
+                fader.ResetFade();
+        }
+            
+        // if (Physics.Raycast(ray, out hit, Mathf.Infinity, Layer))
+        // {
+        //         currentObjectToFade = hit.collider.GetComponent<ObjectFader>();
+        //         currentObjectToFade.Fade();
+        // }
     }
 
    /* private void OnDrawGizmos()
--- Assets/Scripts/Objects/ObjectFader.cs
+++ Assets/Scripts/Objects/ObjectFader.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class ObjectFader : MonoBehaviour
 {
-
+    [SerializeField] private Material originalMaterial;
+    [SerializeField] private Material fadeableMaterial;
     [SerializeField] private float fadeSpeed;
     [SerializeField] private float fadeAmount;
-
+    public bool ShouldFade { get; set; }
     private const float DefaultOpacity = 1f;
+    private const float FadeOpacity = .2f;
+    private float _opacity = DefaultOpacity;
     private Material mat;
     Color currentColor;
 
@@ -18,18 +21,48 @@ public class ObjectFader : MonoBehaviour
         currentColor = mat.color;
     }
 
+    void FixedUpdate()
+    {
+        if (ShouldFade && _opacity > FadeOpacity)
+        {
+            mat = fadeableMaterial;
+            GetComponent<Renderer>().material = mat;
+            _opacity -= fadeSpeed * Time.fixedDeltaTime;
+            mat.color = new Color(currentColor.r, currentColor.g, currentColor.b,_opacity);
+            
+        }
+        else if (!ShouldFade && _opacity < DefaultOpacity)
+        {
+            mat = fadeableMaterial;
+            GetComponent<Renderer>().material = mat;
+            _opacity += fadeSpeed * Time.fixedDeltaTime;
+            mat.color = new Color(currentColor.r, currentColor.g, currentColor.b,_opacity);
+        }
+        else if (!ShouldFade && _opacity >= DefaultOpacity)
+        {
+            mat = originalMaterial;
+            GetComponent<Renderer>().material = mat;
+        }
+        
+        
+    }
+
     public void Fade()
     {
         Color newColor = new Color(currentColor.r, currentColor.g, currentColor.b,
             Mathf.Lerp(currentColor.a, fadeAmount, fadeSpeed * Time.deltaTime));
-        mat.color = newColor;
+       // mat.color = new Color(currentColor.r, currentColor.g, currentColor.b,_opacity);
+        Debug.Log($"Opacity is {_opacity}");
+        //mat.color = newColor;
     }
+    
 
     public void ResetFade()
     {
         Color newColor = new Color(currentColor.r, currentColor.g, currentColor.b,
             Mathf.Lerp(currentColor.a, DefaultOpacity, fadeSpeed * Time.deltaTime));
-        mat.color = newColor;
+       // mat.color = new Color(currentColor.r, currentColor.g, currentColor.b,_opacity);
+       // mat.color = newColor;
     }
 
 }
